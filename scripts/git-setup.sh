3466a53df00b52b54ac687a536391fbea5ec311c
catabi <leriex123@gmail.com>
2026-08-09 17:27:02 +0200

    updating git setup script

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+cd ~/.dots/
 git init
 git add .
+gh auth login
 git remote set-url origin git@github.com:catabi/nixos-dotfiles.git
 sudo git config --global --add safe.directory /home/catabi/.dots
 git push -u origin main
