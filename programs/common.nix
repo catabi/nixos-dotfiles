c80216eacc50008c2b3d784078f25b59f9af29cb
catabi <leriex123@gmail.com>
2026-08-09 21:42:22 +0200

    meow

--- programs/common.nix
+++ programs/common.nix
@@ -194,7 +194,7 @@
 
   programs.hyprland = {
     enable = false;
-    xwayland.enable = true;
+    #xwayland.enable = true;
     #package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     #portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
