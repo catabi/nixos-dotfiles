6f1381f83c4919cda1d44fe60310c712c859286b
catabi <leriex123@gmail.com>
2026-08-20 12:43:17 +0200

    mew

--- programs/yazi/yazi.nix
+++ programs/yazi/yazi.nix
@@ -8,14 +8,7 @@
   programs.yazi = {
     enable = true;
     plugins = {
-      drag = pkgs.yaziPlugins.drag;
-      chmod = pkgs.yaziPlugins.chmod;
-      mount = pkgs.yaziPlugins.mount;
-      bypass = pkgs.yaziPlugins.bypass;
-      compress = pkgs.yaziPlugins.compress;
-      bookmarks = pkgs.yaziPlugins.bookmarks;
-      wl-clipboard = pkgs.yaziPlugins.wl-clipboard;
-      recycle-bin = pkgs.yaziPlugins.recycle-bin;
+      inherit (pkgs.yaziPlugins) chmod mount bypass compress bookmarks wl-clipboard recycle-bin;
     };
     settings = {
       yazi = lib.importTOML ./yazi.toml;
